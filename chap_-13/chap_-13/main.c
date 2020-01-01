cdc8d987596cf8c592b86b5bbb9abd8c6996a405
JaranF Work-MBP <jaranf_dont-email-me@gmx.co.uk>
2020-01-01 18:51:17 +0000

    Chapeter 13: Advanced topics

---
+++ chap_-13/chap_-13/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  chap_-13
+//
+//  Created by JaranF Work-MBP on 01/01/2020.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    // Advanced topics.
+    printf("Hello, World!\n");
+    return 0;
+}
